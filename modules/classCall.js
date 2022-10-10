fde4d7228a2ee9057c1e690198f4c2a8b9f3d892
rebaz36 <rebaz415@gmail.com>
2022-10-11 00:15:30 +0300

    delete duplicate

--- modules/classCall.js
+++
@@ -1,5 +0,0 @@
-import API from './mainClass.js';
-
-const api = new API();
-
-export default api;
--- modules/elementsDOM.js
+++
@@ -1,5 +0,0 @@
-export const list = document.getElementById('list');
-export const submit = document.getElementById('submit');
-export const score = document.getElementById('score');
-export const name = document.getElementById('name');
-export const refresh = document.getElementById('refresh');
