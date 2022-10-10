179a4a979e0f8c8a072fc8734ede17ee192976d9
rebaz36 <rebaz415@gmail.com>
2022-10-11 00:13:38 +0300

    add dom elements

---
+++ modules/elementsDOM.js
@@ -0,0 +1,5 @@
+export const list = document.getElementById('list');
+export const submit = document.getElementById('submit');
+export const score = document.getElementById('score');
+export const name = document.getElementById('name');
+export const refresh = document.getElementById('refresh');
